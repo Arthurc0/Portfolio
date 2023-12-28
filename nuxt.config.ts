2f1254589488e793b0c6b8e1f6d10434f0d1fc15
Arthur <chaff.arthur@gmail.com>
2023-12-28 14:57:48 +0100

    nuxt config

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+export default defineNuxtConfig({
+    devtools: {
+        enabled: true
+    },
+    typescript: {
+        strict: true,
+        typeCheck: true
+    },
+    modules: [
+        '@nuxtjs/tailwindcss'
+    ]
+});
