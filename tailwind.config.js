63e44c87252f6bbb6ad18b0e4518a47bf738a2cf
Arthur <chaff.arthur@gmail.com>
2023-12-28 16:31:24 +0100

    Custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 export default {
     theme: {
+        colors: {
+            'white': '#F2F0ED',
+            'black': '#1B1B1B'
+        },
         transitionDuration: {
             '200': '200ms'
-        }
+        },
+        fontFamily: {
+            'primary': ['"Noto Serif Display"', 'serif'],
+            'secondary': ['Inter', 'sans-serif']
+        },
     }
 }
