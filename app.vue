02a59972ec0656c19f31564243462c46a2903f19
Arthur <chaff.arthur@gmail.com>
2023-12-28 15:20:24 +0100

    add images

---
+++ app.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="text-red-500">test tailwind</div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
         typeCheck: true
     },
     modules: [
-        '@nuxtjs/tailwindcss'
+        '@nuxtjs/tailwindcss',
+        '@nuxt/image'
     ]
 });
