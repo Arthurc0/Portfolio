672a4b7596a996a29061c019981c40cb07884b3a
Arthur <chaff.arthur@gmail.com>
2024-01-01 16:23:30 +0100

    update tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,35 @@
 export default {
     theme: {
-        colors: {
-            'white': '#F2F0ED',
-            'black': '#1B1B1B'
+        extend: {
+            colors: {
+                'white': '#F2F0ED',
+                'black': '#1B1B1B',
+                'figma': '#F24E1E',
+                'node': '#339933',
+                'jira': '#0052CC',
+                'sass': '#CC6699',
+                'nuxt': '#00DC82',
+                'vscode': '#007ACC',
+                'react': '#61DAFB',
+                'git': '#F05032',
+                'linear': '#5E6AD2',
+                'tailwind': '#06B6D4',
+                'typescript': '#3178C6',
+                'javascript': '#F7DF1E',
+                'vue': '#4FC08D',
+                'docker': '#2496ED'
+            }
         },
         transitionDuration: {
             '200': '200ms'
         },
         fontFamily: {
-            'primary': ['"Noto Serif Display"', 'serif'],
+            'primary': ['"Noto Serif"', 'serif'],
             'secondary': ['Inter', 'sans-serif']
         },
-        zIndex: {}
+        zIndex: {
+            'competences': '950',
+            'navbar': '900'
+        }
     }
 }
